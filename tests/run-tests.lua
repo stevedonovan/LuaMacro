2a4757284e3d3fdb8cea9fad37d958845bae8762
steve donovan <steve.j.donovan@gmail.com>
2011-06-26 19:44:06 +0200

    all tests pass with Lua 5.2

--- tests/run-tests.lua
+++ tests/run-tests.lua
@@ -1,11 +1,16 @@
 -- similar syntax to tests.bat, but more portable and aware of errors.
 require_ 'forall'
 require_ 'qw'
+local lua52 = _VERSION:match '5.2'
+local lua51 = not lua52
 def_ put io.stderr:write
+
+local luam = lua51 and 'luam' or 'luam52'
 
 function run (f)
   put(f,': ')
-  if os.execute('luam '..f) ~= 0 then
+  local res = os.execute(luam..' '..f)
+  if (lua52 and not res) or (lua51 and res ~= 0) then
     put 'failed!\n'
     os.exit(1)
   else
