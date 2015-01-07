3a10aa8d69446c7ad8b1daebd9831761ff769701
Gary V. Vaughan <gary@gnu.org>
2015-01-07 04:32:11 +0000

    macro: display a colon after error line number.
    * macro.lua (error): Many standard tools and editors rely on error
    output format `filename:line: message' for full features.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- macro.lua
+++ macro.lua
@@ -322,7 +322,7 @@ M.please_throw = false
 --- macro error messages.
 -- @param msg the message: will also have file:line.
 function M.error(msg)
-    msg = M.filename..':'..lexer.line..' '..msg
+    msg = M.filename..':'..lexer.line..': '..msg
     if M.please_throw then
         error(msg,2)
     else
